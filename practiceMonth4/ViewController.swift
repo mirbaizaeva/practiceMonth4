6d00e2dc504849e1b9a61741347e38314d8b2897
Nurjamal <n.mirbaizaeva@gmail.com>
2023-04-29 20:11:54 +0600

    Initial Commit

---
+++ practiceMonth4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceMonth4
+//
+//  Created by Nurjamal Mirbaizaeva on 29/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
